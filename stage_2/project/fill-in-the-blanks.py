462cbaa45cb8ad1f3b851ff8cc5f108e584afc7d
gvsvarun <gvsvarun@gmail.com>
2016-08-28 01:01:56 +0530

    incomplete modifications

--- stage_2/project/fill-in-the-blanks.py
+++ stage_2/project/fill-in-the-blanks.py
@@ -1,36 +1,33 @@
 # IPND Stage 2 Final Project
 
-# You've built a Mad-Libs game with some help from Sean.
-# Now you'll work on your own game to practice your skills and demonstrate what you've learned.
-
-# For this project, you'll be building a Fill-in-the-Blanks quiz.
-# Your quiz will prompt a user with a paragraph containing several blanks.
-# The user should then be asked to fill in each blank appropriately to complete the paragraph.
-# This can be used as a study tool to help you remember important vocabulary!
-
-# Note: Your game will have to accept user input so, like the Mad Libs generator,
-# you won't be able to run it using Sublime's `Build` feature.
-# Instead you'll need to run the program in Terminal or IDLE.
-# Refer to Work Session 5 if you need a refresher on how to do this.
-
-# To help you get started, we've provided a sample paragraph that you can use when testing your code.
-# Your game should consist of 3 or more levels, so you should add your own paragraphs as well!
-
 sample = '''A ___1___ is created with the def keyword. You specify the inputs a ___1___ takes by
 adding ___2___ separated by commas between the parentheses. ___1___s by default return ___3___ if you
 don't specify the value to return. ___2___ can be standard data types such as string, number, dictionary,
 tuple, and ___4___ or can be more complicated such as objects and lambda functions.'''
 
-# The answer for ___1___ is 'function'. Can you figure out the others?
 
-# We've also given you a file called fill-in-the-blanks.pyc which is a working version of the project.
-# A .pyc file is a Python file that has been translated into "byte code".
-# This means the code will run the same as the original .py file, but when you open it
-# it won't look like Python code! But you can run it just like a regular Python file
-# to see how your code should behave.
+# All variables that are used in the function
+quiz_level_option = ['Easy','Medium','Hard']
+intro_statement = 'Hi! Welcome to IPND Python Quiz. Please select one of the below options'
+intro_quiz_options = "/".join(quiz_level_option)
 
-# Hint: It might help to think about how this project relates to the Mad Libs generator you built with Sean.
-# In the Mad Libs generator, you take a paragraph and replace all instances of NOUN and VERB.
-# How can you adapt that design to work with numbered blanks?
+prob_statement1 = 'Problem statement 1'
+prob_statement2 = 'Problem statement 2'
+prob_statement3 = 'Problem statement 3'
 
-# If you need help, you can sign up for a 1 on 1 coaching appointment: https://calendly.com/ipnd1-1/20min/
+
+# Greet the user and obtain the difficulty level of the quizz the user wants to try
+# Depending on the user difficulty level generate the problem statement
+# Ask the user to provide how many number of chances would be needed
+# Show the problem statement and ask the user to provide an input
+# each problem statement will have its predefined options mapped to it
+# checking if user input is correct response
+# bidding good bye to the user
+
+# The main function to execute when the application loads
+def start_ipnd_quiz():
+    print intro_statement
+    print intro_quiz_options
+    return ''
+
+print start_ipnd_quiz()
